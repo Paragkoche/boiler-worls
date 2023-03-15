c2a2a10299318446f8e4ebca1d6fc9242a120239
Paragkoche <paragkoche0@gmail.com>
2023-03-15 08:09:18 +0530

    add

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,11 +4,13 @@ import {config} from "dotenv";
 import  enstitys from "../model"
 config()
 export default new DataSource({
-    type:'mysql',
-    host:process.env.DB_HOST,
-    username:process.env.DB_USER,
-    database:process.env.DB_NAME,
-    port:parseInt(process.env.DB_PORT || ''),
+    type:'postgres',
+    url:"postgresql://parag:_2wl-so2S0KnHktLjZJOrQ@discord-3838.6xw.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full",
+    
+    // host:process.env.DB_HOST,
+    // username:process.env.DB_USER,
+    // database:process.env.DB_NAME,
+    // port:parseInt(process.env.DB_PORT || ''),
     logging:'all',
     synchronize:true,
     migrationsRun: true,
